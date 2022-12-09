4e177305364a4b34a4595767e38dcd0eb0b8ddf4
dhanushvarman <dhanushvarmanj@gmail.com>
2022-12-09 11:28:06 +0530

    api change

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const config = {
 
     // api : "http://localhost:3000"
-    api : "https://reset-password-server.vercel.app"
+    // api : "https://reset-password-index.vercel.app"
+    api : "https://reset-password-server.onrender.com"
 }
